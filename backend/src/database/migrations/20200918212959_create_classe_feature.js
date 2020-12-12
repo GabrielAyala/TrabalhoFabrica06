12391f1eabc25499058161863f9d71c61c4f1e48
Crystian Jordan <crystian.jordan@catolicasc.org.br>
2020-12-11 22:42:19 -0300

    database filled

--- backend/src/database/migrations/20200918212959_create_classe_feature.js
+++ backend/src/database/migrations/20200918212959_create_classe_feature.js
@@ -1,16 +1,14 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("classe_feature", function (table) {
+    table.increments("id_classe_feature").primary();
+    table.string("nome_classe_feature").notNullable();
+    table.string("descricao_classe_feature").notNullable();
+    table.integer("level").notNullable();
 
-exports.up = function(knex) {
-  return knex.schema.createTable('classe_feature', function (table) {
-    table.increments('id_classe_feature').primary();
-    table.string('nome_classe_feature').notNullable();
-    table.string('descricao_classe_feature').notNullable();
+    table.integer("classe_id");
 
-    table.integer('classe_id');
-
-    table.foreign('classe_id').references('id_classe').inTable('raca');
+    table.foreign("classe_id").references("id_classe").inTable("raca");
   });
 };
 
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {};
