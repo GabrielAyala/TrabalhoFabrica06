77ec78fa75873ddb4b9924c543fdbd4f646b5f8c
Gabriel Ayala <gabriel123ayala@gmail.com>
2020-12-16 21:47:27 -0300

    Puxando as fichas do backend para o frontend

--- frontend/src/pages/Fichas/index.js
+++ frontend/src/pages/Fichas/index.js
@@ -2,17 +2,21 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
-import api from '../../services/api';
 import './styles.css';
+import Axios from 'axios';
 
 function Fichas(){
 
-    const [incidentes, setIncidentes] = useState([]);
+    const [data, setData] = useState([]);
 
     useEffect(() => {
-        api.get('fichas').then(response => {
-            setIncidentes(response.data);
-        });
+        Axios
+        .get("http://localhost:3333/fichas")
+        .then((api) => {
+            setData(api.data);
+            console.log(api);
+        })
+        .catch(() => console.log("error"));
     }, []);
 
     return (
@@ -27,16 +31,43 @@ function Fichas(){
             <h1>Fichas Cadastradas</h1>
 
             <ul>
-                {incidentes.map(incidentes => (
+                {data.map(data => (
                     <li>
                         <strong>Nome do personagem:</strong>
-                        <p>{incidentes.nomePersonagem}</p>
+                        <p>{data.nomePersonagem}</p>
                     
                         <strong>Raça</strong>
-                        <p>{incidentes.id_raca_ficha}</p>
-                    
-                        <strong>Classe:</strong>
-                        <p>Mago</p>
+                        <p>{data.id_raca_ficha}</p>
+
+                        <strong>Xp</strong>
+                        <p>{data.xp}</p>
+
+                        <strong>Antecedente</strong>
+                        <p>{data.antecedente}</p>
+
+                        <strong>Vida Maxima</strong>
+                        <p>{data.vida_maxima}</p>
+
+                        <strong>Força</strong>
+                        <p>{data.forca}</p>
+
+                        <strong>Destreza</strong>
+                        <p>{data.destreza}</p>
+
+                        <strong>Constituição</strong>
+                        <p>{data.constituicao}</p>
+
+                        <strong>Inteligência</strong>
+                        <p>{data.inteligencia}</p>
+
+                        <strong>Sabedoria</strong>
+                        <p>{data.sabedoria}</p>
+
+                        <strong>Carisma</strong>
+                        <p>{data.carisma}</p>
+
+                        <strong>Level</strong>
+                        <p>{data.level}</p>
                     
                         <button type="button">
                         <FiTrash2 size={20} color=" #a8a8b3" />
